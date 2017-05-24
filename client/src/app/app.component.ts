79442212c860a2bdf157c6ac19877e33bc2c59c4
Amod Upadhye <amod.upadhye@gmail.com>
2017-05-24 00:51:40 -0500

    Initialized Task Component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {TaskService} from './services/tasks.service';
 
 @Component({
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: 'app.component.html',
+  //need to add service as providers
+  providers:[TaskService],
 })
 export class AppComponent  { name = 'Angular'; }
--- client/src/app/components/tasks/tasks.component.ts
+++ client/src/app/components/tasks/tasks.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {TaskService} from '../../services/tasks.service';
 
 @Component({
-    moduleId: module.id,
+  moduleId: module.id,
   selector: 'tasks',
   templateUrl: 'tasks.component.html',
 })
-export class TasksComponent  { name = 'Angular'; }
+export class TasksComponent  {
+
+  //add constructor with a call to taskService
+  constructor(private taskService:TaskService){}
+ }
